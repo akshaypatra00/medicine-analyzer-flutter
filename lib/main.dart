8269b05da80aff589f1ff2b18eb238aa16f4f5a1
Akshay patra <akshaykumarpatra00@gmail.com>
2026-02-06 23:10:36 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,5 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'core/theme/app_theme.dart';
 import 'core/constants/app_strings.dart';
-import 'presentation/pages/splash_page.dart';
-import (key: key);
 
 }
