f159563ebbe8384cc867b2449080d422518b4860
Akshay patra <akshaykumarpatra00@gmail.com>
2026-02-06 23:09:17 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'core/theme/app_theme.dart';
 import 'core/constants/app_strings.dart';
 import 'presentation/pages/splash_page.dart';
-import 'presentation/pages/home_page.dart';
-import 'presentation/pages/history_page.dart';
-import 'presentation/pages/settings_page.dart';
-import 'presentation/providers/app_providers.dart';
-
-  runApp(const ProviderScope(child: MedicineAnalyzerApp()));
-}
-
-class MedicineAnalyzerApp extends ConsumerWidget {
-  const MedicineAnalyzerApp({Key? key}) : super(key: key);
+import (key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
